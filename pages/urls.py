d721975c6f70e554c01833aa7c43e54b94704b81
Ricardo Ramos Pereira <ricardoramospereira33@gmail.com>
2022-11-16 10:08:43 -0300

    navigation_link

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import index
+from .views import index, about, cars, service, contact
 
 urlpatterns = [
-    path("", index, name='index')
+    path("", index, name='index'),
+    path("about/", about, name='about'),
+    path("cars/", cars, name='cars'),
+    path("service/", service, name='service'),
+    path("contact/", contact, name='contact'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'pages/index.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def cars(request):
+    return render(request, 'pages/cars.html')
+
+def service(request):
+    return render(request, 'pages/service.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')
