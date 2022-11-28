8b89f47611e3920913b71316772bed5ea0fe02a6
Ricardo Ramos Pereira <ricardoramospereira33@gmail.com>
2022-11-28 15:16:20 -0300

    pagination

--- pages/admin.py
+++ pages/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from .models import Team
 from django.utils.html import format_html
 
-# Register your models here.
+# add new action
+
+@admin.action(description='Alterar função para designer')
+def action_alterar_funcao_designer(modeladmin, request, queryset):
+     print(queryset)
+
+
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
      def Thumbnail(self, object):
@@ -13,3 +19,5 @@ class TeamAdmin(admin.ModelAdmin):
      list_display = ("Thumbnail", "first_name", "last_name", "designation", "created_date" )
      search_fields = ('first_name', 'designation')
      list_filter = ('designation',)
+     # new action
+     actions = [action_alterar_funcao_designer]
