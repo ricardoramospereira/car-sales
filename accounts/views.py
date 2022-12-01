8845d178958d17d2592d98b191539dd895ee2687
Ricardo Ramos Pereira <ricardoramospereira33@gmail.com>
2022-12-01 11:36:45 -0300

    registry_area

---
+++ accounts/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+# Create your views here.
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def register(request):
+    if request.method == 'POST':
+        messages.error(request, 'Erro ao criar conta')
+        return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
+
+def logout(request):
+    return redirect('index')
--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "pages.apps.PagesConfig",
     "cars.apps.CarsConfig",
+    "accounts.apps.AccountsConfig",
     #test
     "ckeditor",
     "django.contrib.humanize",
@@ -127,3 +128,9 @@ STATIC_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# mensages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
